bdf387129a301a011a3a2224f7be6bc3db106bb9
houtianyu <houtian_yu@yeah.net>
2019-05-29 08:16:56 +0800

    桌面管里工具

--- InterfaceCall/interfaceCall.py
+++ InterfaceCall/interfaceCall.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from OtherJobs.otherJob import OtherJobs
-import os,sys,win32process,win32gui
+import os,sys,win32process,win32gui,time,win32api,win32con
+from pywinauto import application
+from pykeyboard import *
+from pymouse import *
+#import SendKeys
 class BaiDuSearch:
     def __init__(self):
         self.dr = webdriver.Chrome()
@@ -20,8 +24,23 @@ class NewFiles:
 class LocateSearch:
     def __init__(self):
         self.inter_other = OtherJobs()
+        self.app = application.Application()
     def locate_search(self,search_content):
-        self.inter_other.input_value(search_content,'Everything',)
+        #self.app.start('E:\\softinstall\\Everything\\Everything.exe')
+        search_all = 'E:\\softinstall\\Everything\\Everything.exe -search ' + search_content
+        search_close = 'E:\\softinstall\\Everything\\Everything.exe -close'
+        os.system(search_all)
+        num_list0 = [18,70]
+        num_list1 = [17,69]
+        self.inter_other.mouse_input_remote_on(num_list0)
+        self.inter_other.mouse_input_remote_up(num_list0)
+        self.inter_other.mouse_input_remote_on(num_list1)
+        self.inter_other.mouse_input_remote_up(num_list1)
+        self.inter_other.save_winfile()
+
+        time.sleep(5)
+        #os.system(search_close)
+
 
 
 
--- OtherJobs/otherJob.py
+++ OtherJobs/otherJob.py
@@ -1,4 +1,8 @@
 import threading,win32com.client,win32api,win32con,win32gui,os,time
+from win32gui import *
+from win32api import *
+from win32process import *
+from win32con import *
 class OtherJobs:
 
     def thread_add(self,func,*args):
@@ -19,36 +23,50 @@ class OtherJobs:
         else:
             print("is not exist")
             return False
-    def mouse_click_order(self,handle):
+    def mouse_click_order(self,handle,num):
         handleDetail = win32gui.GetWindowRect(handle)
-        x = handleDetail[0] + 100
+        x = handleDetail[0]
         y = handleDetail[1]
+        if num == 0:
+            x = handleDetail[0] - 300
+            y = handleDetail[1] - 200
         time.sleep(0.5)
         win32api.SetCursorPos((x, y))
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
         time.sleep(0.05)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-    def mouse_input_remote(self,num):
-        win32api.keybd_event(num, 0, 0, 0)
-        win32api.keybd_event(num, 0, win32con.KEYEVENTF_KEYUP, 0)
+    def mouse_input_remote_on(self,list_num):
+        for num in list_num:
+            win32api.keybd_event(num, 0, 0, 0)
+    def mouse_input_remote_up(self,list_num):
+        for num in list_num:
+            win32api.keybd_event(num, 0, win32con.KEYEVENTF_KEYUP, 0)
+    def mouse_input_remote_onup(self,outstr):
+        win32api.keybd_event(outstr, 0, 0, 0)
+        win32api.keybd_event(outstr, 0, win32con.KEYEVENTF_KEYUP, 0)
     def input_stream_remote(self,strip_ckcode):
         for i, ch in enumerate(strip_ckcode):
+            print(ch)
             if ch == '.':
                 outstr = 110
             else:
-                outstr = ord(ch)
-            self.mouse_input_remote(outstr)
-    def input_value(self,search_content,windowsone,class_two,windowstwo=None):
-        hld = win32gui.FindWindow(None,windowsone)
-        if hld:
-            pass
-        else:
-            exe = windowsone + '.exe'
-            exe_path = 'E:\\softinstall\\Everything\\' + exe
-            os.system(exe_path)
-        hld = win32gui.FindWindow(None, windowsone)
+                outstr = ord(ch)#ord
+            print(outstr)
+            self.mouse_input_remote_onup(outstr)#outstr
+    def save_winfile(self):
         time.sleep(0.5)
-        hld_two = win32gui.FindWindowEx(hld, None, class_two,windowstwo)  # 获取hld下第一个为edit控件的句柄
-        self.mouse_click_order(hld_two)
-        self.input_stream_remote(search_content)
+        hld = win32gui.FindWindow(None,'导出结果列表')
         time.sleep(0.5)
+        hld_filename = win32gui.FindWindowEx(hld,None,u'Button',u'保存(&S)')
+        #hld_filepath = win32gui.FindWindowEx(hld,None,'ToolbarWindow32','地址: 桌面')
+        print(hld)
+        #print(hld_filepath)dp
+        print(hld_filename)
+        self.mouse_click_order(hld_filename,0)
+        #hld_filepath_num = [68,58,92,80,121,116,104,111,110,92,72,111,117,116,105,111,110,84,111,111,108,115,92,70,105,108,101,115]
+        path = 'D:\Python\HoutianTools\Files\search_result.txt'
+        self.input_stream_remote(path)
+        time.sleep(2)
+if __name__ == '__main__':
+    a = OtherJobs()
+    a.save_winfile()
